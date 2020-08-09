b091eb09d895ba106abcdfd998055f37d37ed6aa
Ayoub El Hayat <aubser12@gmail.com>
2020-08-10 00:41:28 +0100

    fix the map size

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 import "./App.css";
-
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 function App() {
   const [lat, setLat] = useState(51.505);
@@ -11,12 +10,12 @@ function App() {
   const [position, setPosition] = useState([lat, lng]);
 
   return (
-    <Map className="map" center={[lat, lng]} zoom={zoom}>
+    <Map className="map" center={position} zoom={zoom}>
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[lat, lng]}>
+      <Marker position={position}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "leaflet/dist/leaflet.css";
-
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
